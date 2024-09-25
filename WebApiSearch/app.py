a23e14fc54c656a918100df781fa88637812fe4d
Piero Valentino Noa Chahuayo <piero.noa@tecsup.edu.pe>
2024-09-25 01:00:21 -0500

    010

--- WebApiSearch/app.py
+++ WebApiSearch/app.py
@@ -84,4 +84,4 @@ def search_users():
     return jsonify([result['user'] for result in results])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
