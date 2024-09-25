c1921ffe149d2922b1ac426fc8fd1db7339aef42
Piero Valentino Noa Chahuayo <piero.noa@tecsup.edu.pe>
2024-09-25 01:29:31 -0500

    013

--- script.sh
+++ script.sh
@@ -6,4 +6,14 @@ cd app
 
 docker compose up -d --build 
 
-./script2.sh
+# Duerme un momento
+sleep 3
+
+# Cambiar al directorio db
+cd db || { echo "Directorio 'db' no encontrado."; exit 1; }
+
+# Copiar el archivo JSON al contenedor
+docker cp WebApi.User.json db:/data/WebApi.User.json
+
+# Ejecutar mongoimport dentro del contenedor
+docker exec -it db /bin/sh -c "mongoimport --db WebApi --collection User --file /data/WebApi.User.json --jsonArray --username root --password example --authenticationDatabase admin"
