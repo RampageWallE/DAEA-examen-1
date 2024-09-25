ceaf9813db24dca4434d4cbc0ced6f44f4449d32
Piero Valentino Noa Chahuayo <piero.noa@tecsup.edu.pe>
2024-09-25 01:31:40 -0500

    013

--- script.sh
+++ script.sh
@@ -17,3 +17,11 @@ docker cp WebApi.User.json db:/data/WebApi.User.json
 
 # Ejecutar mongoimport dentro del contenedor
 docker exec -it db /bin/sh -c "mongoimport --db WebApi --collection User --file /data/WebApi.User.json --jsonArray --username root --password example --authenticationDatabase admin"
+
+cd .. 
+
+cd ..
+
+rm -rf app 
+
+tree 
